ad73fbd23803e181122a1881426b4396727c0055
Rob Nichols <robnichols@warwickshire.gov.uk>
2016-10-18 11:20:28 +0100

    Add permissible states method

--- test/linear_work_flow/work_flow_test.rb
+++ test/linear_work_flow/work_flow_test.rb
@@ -82,16 +82,54 @@ module LinearWorkFlow
     end
 
     def test_at
-      work_flow = WorkFlow.at valid_state
+      work_flow = TempWorkFlow.at valid_state
       assert_equal valid_state, work_flow.state
     end
 
+    def test_forward_state
+      assert_equal states[1], work_flow.forward_state
+    end
+
+    def test_forward_state_when_last
+      work_flow states.last
+      assert_equal nil, work_flow.forward_state
+    end
+
+    def test_back_state
+      work_flow states[1]
+      assert_equal states[0], work_flow.back_state
+    end
+
+    def test_bach_state_when_first
+      assert_equal nil, work_flow.back_state
+    end
+
+    def test_permissible_states
+      work_flow states[1]
+      assert_equal [0, 1, 2].collect{|n| states[n]}, work_flow.permissible_states
+    end
+
+    def test_permissible_states_when_first
+      assert_equal [0, 1].collect{|n| states[n]}, work_flow.permissible_states
+    end
+
+    def test_permissible_states_when_last
+      work_flow states.last
+      assert_equal [-2, -1].collect{|n| states[n]}, work_flow.permissible_states
+    end
+
+    class TempWorkFlow < WorkFlow
+      def self.states
+        [:one, :two, :three, :four]
+      end
+    end
+
     def work_flow(state = nil)
-      @work_flow ||= WorkFlow.new(state)
+      @work_flow ||= TempWorkFlow.new(state)
     end
 
     def states
-      WorkFlow.states
+      TempWorkFlow.states
     end
 
     def valid_state
